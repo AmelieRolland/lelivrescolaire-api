55313aee6c85548f5753744ec7828e387e6332e8
Amelie <amelierolland.v@gmail.com>
2024-08-05 16:11:49 +0200

    modifications
    modifications gestion erreurs, et remplacement d'image si elle n'existe pas

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -5,6 +5,9 @@ import LevelsList from './LevelsList';
 import './Booklist.css';
 
 const BookList = () => {
+
+    const genericImageUrl ='/img/book.png';
+
     const [selectedLevel, setSelectedLevel] = useState();
     const [isLoading, setIsLoading] = useState(false);
 
@@ -28,23 +31,21 @@ const BookList = () => {
         return (
             <p>En attente du chargement</p>
         );
-    } else if (!Array.isArray(books) || books.length === 0 ){
+    } else if (!books || books.length === 0 ){
         return (
-            <p>Oups! Aucuns livres disponibles pour le moment</p>
+            <p>Oups! Aucun livre disponible pour le moment</p>
         )
     }
 
     return (
         <>
-            {/* <LevelsList onSelectLevel={handleSelectLevel} /> */}
-
             <div className='container mx-auto px-4'>
                 <h1 className='permanent-marker text-3xl py-16'>Nos ouvrages {selectedLevel} :</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     {filteredBooks.map((book) => (
                         <div key={book.id} className={"group flex flex-row transition duration-700 ease-in-out max-w-full h-auto border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700" + (book.valid === true ? ' bg-white' : ' hidden')}>
                             <div className="flex-shrink-0 w-1/3">
-                                <img src={book.urlLite} alt={book.displayTitle} className="w-full h-full object-cover rounded-l-lg transform transition-transform duration-700 ease-in-out group-hover:rotate-6" />
+                                <img src={book.urlLite ? book.urlLite : genericImageUrl} alt={book.displayTitle} className="w-full h-full object-cover rounded-l-lg transform transition-transform duration-700 ease-in-out group-hover:rotate-6" />
                             </div>
                             <div className='flex flex-col justify-between p-4 w-2/3'>
                                 <div>
@@ -53,7 +54,7 @@ const BookList = () => {
                                     </a>
                                 </div>
                                 <div className='mt-2'>
-                                    <a href={`/book/${book.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#ffa800] rounded-lg hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    <a href={`/book/${book.id}`} title='découvrir le livre' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#ffa800] rounded-lg hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         Ouvrir
                                         <svg className="rtl:rotate-180 w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
